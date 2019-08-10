f40938a94a766ed4c07b84ded54b63b0914af1e0
Coyeah <Coyeah_chen@outlook.com>
2019-08-10 22:36:25 +0800

    updated: website config

--- config/website.js
+++ config/website.js
@@ -1,6 +1,13 @@
 const website = {
+  title: 'oops-project',
   name: 'oops-project',
   author: 'Coyeah Chen',
+  owner: 'coyeah',
+  repo: 'Create a quick start project',
+  description: 'Create a quick start project',
+  github: 'https://github.com/Coyeah/oops-project',
+  licenses: "http://creativecommons.org/licenses/by-nc/4.0/",
+  since: 2018,
 };
 
 module.exports = website;

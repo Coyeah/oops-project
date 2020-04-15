7bb9c6781d39eef43c4fb5c728809300759b5196
Coyeah <Coyeah_chen@outlook.com>
2020-04-15 11:03:30 +0800

    feat: config

---
+++ typings.d.ts
@@ -0,0 +1,9 @@
+declare module '*.css';
+declare module '*.png';
+declare module '*.svg';
+
+declare module '*.less';
+declare module '*.css';
+
+declare interface AnyObject { [name: string]: any }
+declare interface StringObject { [name: string]: string }
